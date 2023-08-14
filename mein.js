88059047f4c3093cdd6eb86479765c30c080e92d
ibrahim <nsism99@gmail.com>
2023-08-14 15:22:07 +0300

    finish

---
+++ mein.js
@@ -0,0 +1,11 @@
+let Names = ["ibrahim","ALI","SAMAH","batal","sultan"]
+console.log(Names);
+console.log(Names[2]);
+Names.unshift("ronaldo")
+console.log(Names);
+Names.push("ittihad")
+console.log(Names);
+Names.shift()
+console.log(Names);
+Names.pop()
+console.log(Names);
